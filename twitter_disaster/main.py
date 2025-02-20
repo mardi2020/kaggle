bcd805d69ebb80dedcbb593bc800948debf3b8b6
mardi2020 <mardi@khu.ac.kr>
2025-02-20 23:23:52 +0900

    chore: 코드 정리

--- twitter_disaster/main.py
+++ twitter_disaster/main.py
@@ -1,15 +1,11 @@
-from preprocess import preprocess
-from load_csv import load
-from tokenizing import tokenize_and_convert
+from load_csv import load, is_already_embedded
+from embedding import start_embedding
+from training import train_model
+
 
 if __name__ == "__main__":
-    # data 불러오기
     train_df, test_df = load()
+    if not is_already_embedded():
+        start_embedding(train_df, test_df)
 
-    train_df = preprocess(train_df)
-    test_df = preprocess(test_df)
-
-    train_df['token_ids'] = train_df['clean_text'].apply(tokenize_and_convert)
-    test_df['token_ids'] = test_df['clean_text'].apply(tokenize_and_convert)
-
-
+    train_model(train_df)
